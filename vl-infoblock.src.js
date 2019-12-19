33afd847e0a92f13711737ede4819022988bed50
wautergu <guy.wauters@vlaanderen.be>
2019-12-19 10:48:24 +0100

    IS-846: first commit

---
+++ vl-infoblock.src.js
@@ -0,0 +1,19 @@
+import { VlElement, define } from '/node_modules/vl-ui-core/vl-core.js';
+
+/**
+ * VlInfoblock
+ * @class
+ * @classdesc 
+ * 
+ * @extends VlElement
+ * 
+ * @property 
+ * 
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-infoblock/releases/latest|Release notes}
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-infoblock/issues|Issues}
+ * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-infoblock.html|Demo}
+ * 
+ */
+export class VlInfoblock extends VlElement(HTMLElement) {}
+
+define('vl-infoblock', VlInfoblock);
