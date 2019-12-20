1616f429dba5e2c83f2d301967ebc43f231c3b8f
wautergu <guy.wauters@vlaanderen.be>
2019-12-20 13:06:48 +0100

    IS-846: Introductie webcomponent vl-ui-infoblock (component + testen)

--- vl-infoblock.src.js
+++ vl-infoblock.src.js
@@ -1,4 +1,5 @@
 import { VlElement, define } from '/node_modules/vl-ui-core/vl-core.js';
+import '/node_modules/vl-ui-icon/vl-icon.js';
 
 /**
  * VlInfoblock
@@ -14,6 +15,48 @@ import { VlElement, define } from '/node_modules/vl-ui-core/vl-core.js';
  * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-infoblock.html|Demo}
  * 
  */
-export class VlInfoblock extends VlElement(HTMLElement) {}
+export class VlInfoblock extends VlElement(HTMLElement) {
+
+    static get _observedAttributes() {
+        return ['titel', 'icon'];
+    }
+
+    constructor() {
+        super(`
+                <style>
+                    @import '../style.css';
+                    @import '/node_modules/vl-ui-icon/style.css';
+                    
+                </style>
+    
+                <section class="vl-infoblock">
+                    <header class="vl-infoblock__header" role="presentation">
+                        <span is="vl-icon" id="infoblock_icon" class="vl-infoblock__header__icon"></span>
+                        <h2 class="vl-infoblock__title" id="infoblock_title"></h2>
+                    </header>
+                    <div class="vl-infoblock__content" id="infoblock_content">
+                       <slot></slot>
+                    </div>  
+                </section>
+            `);
+    }
+
+    _titelChangedCallback(oldValue, newValue) {
+        this._titleElement.innerText = newValue;
+    }
+
+    _iconChangedCallback(oldValue, newValue) {
+        this._iconElement.setAttribute("icon", newValue);
+    }
+
+    get _titleElement() {
+        return this._element.querySelector('#infoblock_title');
+    }
+
+    get _iconElement() {
+        return this._element.querySelector('#infoblock_icon');
+    }
+
+}
 
 define('vl-infoblock', VlInfoblock);
