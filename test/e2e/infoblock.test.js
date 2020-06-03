946fa542dc3eda093ad8da28e2d18c478c4aaa11
coemans <tom.coemans@gmail.com>
2020-06-03 13:38:12 +0200

    Introductie vl-ui-demo en ESlint (#32)

--- test/e2e/infoblock.test.js
+++ test/e2e/infoblock.test.js
@@ -1,67 +1,66 @@
 
-const { assert, driver } = require('vl-ui-core').Test.Setup;
+const {assert, driver} = require('vl-ui-core').Test.Setup;
 const VlInfoblockPage = require('./pages/vl-infoblock.page');
 
 describe('vl-infoblock', async () => {
-    const vlInfoblockPage = new VlInfoblockPage(driver);
+  const vlInfoblockPage = new VlInfoblockPage(driver);
 
-    before(() => {
-        return vlInfoblockPage.load();
-    });
+  before(() => {
+    return vlInfoblockPage.load();
+  });
 
-    it('als gebruiker kan ik de titel van een infoblock zien', async() => {
-        const iconblock = await vlInfoblockPage.getContactBlock();
-        const title = await iconblock.getTitleSlotElements();
-        await assert.eventually.equal(title[0].getText(), 'Contactenlijst');
-    });
+  it('als gebruiker kan ik de titel van een infoblock zien', async () => {
+    const iconblock = await vlInfoblockPage.getContactBlock();
+    const title = await iconblock.getTitleSlotElements();
+    await assert.eventually.equal(title[0].getText(), 'Contactenlijst');
+  });
 
-    it('als gebruiker kan ik de content van een infoblock zien', async() => {
-        const iconblock = await vlInfoblockPage.getContactBlock();
-        const content = await iconblock.getContentSlotNodes();
-        assert.equal(content[0].textContent, 'Hieronder bevindt zich een overzicht van al uw contacten binnen de Vlaamse Overheid.');
-    });
+  it('als gebruiker kan ik de content van een infoblock zien', async () => {
+    const iconblock = await vlInfoblockPage.getContactBlock();
+    const content = await iconblock.getContentSlotNodes();
+    assert.equal(content[0].textContent, 'Hieronder bevindt zich een overzicht van al uw contacten binnen de Vlaamse Overheid.');
+  });
 
-    it('als gebruiker kan ik een infoblock van type contacten herkennen', async() => {
-        const iconblock = await vlInfoblockPage.getContactBlock();
-        await assert.eventually.isTrue(iconblock.isContact());
-    });
+  it('als gebruiker kan ik een infoblock van type contacten herkennen', async () => {
+    const iconblock = await vlInfoblockPage.getContactBlock();
+    await assert.eventually.isTrue(iconblock.isContact());
+  });
 
-    it('als gebruiker kan ik een infoblock van type publicaties herkennen', async() => {
-        const iconblock = await vlInfoblockPage.getPublicationsBlock();
-        await assert.eventually.isTrue(iconblock.isPublication());
-    });
+  it('als gebruiker kan ik een infoblock van type publicaties herkennen', async () => {
+    const iconblock = await vlInfoblockPage.getPublicationsBlock();
+    await assert.eventually.isTrue(iconblock.isPublication());
+  });
 
-    it('als gebruiker kan ik een infoblock van type faq herkennen', async() => {
-        const iconblock = await vlInfoblockPage.getFaqBlock();
-        await assert.eventually.isTrue(iconblock.isFaq());
-    });
+  it('als gebruiker kan ik een infoblock van type faq herkennen', async () => {
+    const iconblock = await vlInfoblockPage.getFaqBlock();
+    await assert.eventually.isTrue(iconblock.isFaq());
+  });
 
-    it('als gebruiker kan ik een infoblock van type nieuws herkennen', async() => {
-        const iconblock = await vlInfoblockPage.getNewsBlock();
-        await assert.eventually.isTrue(iconblock.isNews());
-    });
+  it('als gebruiker kan ik een infoblock van type nieuws herkennen', async () => {
+    const iconblock = await vlInfoblockPage.getNewsBlock();
+    await assert.eventually.isTrue(iconblock.isNews());
+  });
 
-    it('als gebruiker kan ik een infoblock van type tijdlijn herkennen', async() => {
-        const iconblock = await vlInfoblockPage.getTimelineBlock();
-        await assert.eventually.isTrue(iconblock.isTimeline());
-    });
+  it('als gebruiker kan ik een infoblock van type tijdlijn herkennen', async () => {
+    const iconblock = await vlInfoblockPage.getTimelineBlock();
+    await assert.eventually.isTrue(iconblock.isTimeline());
+  });
 
-    it('als gebruiker kan ik een infoblock van type vraag herkennen', async() => {
-        const iconblock = await vlInfoblockPage.getQuestionBlock();
-        await assert.eventually.isTrue(iconblock.isQuestion());
-    });
+  it('als gebruiker kan ik een infoblock van type vraag herkennen', async () => {
+    const iconblock = await vlInfoblockPage.getQuestionBlock();
+    await assert.eventually.isTrue(iconblock.isQuestion());
+  });
 
-    it('als gebruiker kan ik een custom icon kiezen', async () => {
-        const iconblock = await vlInfoblockPage.getCustomIconBlock();
-        const icon = await iconblock.getIcon();
-        await assert.eventually.equal(icon.getType(), 'calendar');
-        await assert.eventually.isNull(iconblock.getType());
-    });
+  it('als gebruiker kan ik een custom icon kiezen', async () => {
+    const iconblock = await vlInfoblockPage.getCustomIconBlock();
+    const icon = await iconblock.getIcon();
+    await assert.eventually.equal(icon.getType(), 'calendar');
+    await assert.eventually.isNull(iconblock.getType());
+  });
 
-    it('als gebruiker kan ik een de titel van een info-block ook in een slot meegeven', async() => {
-        const iconblock = await vlInfoblockPage.getContactSlotBlock();
-        const title = await iconblock.getTitleSlotElements();
-        await assert.eventually.equal(title[0].getText(), 'Titel via slot');
-    });
-
+  it('als gebruiker kan ik een de titel van een info-block ook in een slot meegeven', async () => {
+    const iconblock = await vlInfoblockPage.getContactSlotBlock();
+    const title = await iconblock.getTitleSlotElements();
+    await assert.eventually.equal(title[0].getText(), 'Titel via slot');
+  });
 });
